b1aaeed64916f1bd01f71b9b625ebdd4452003c1
차지훈 <jhchv@naver.com>
2019-06-28 22:55:29 +0900

    프로젝트 생성

---
+++ src/main/java/jhchv/searchplace/SearchPlaceApplication.java
@@ -0,0 +1,13 @@
+package jhchv.searchplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchPlaceApplication.class, args);
+    }
+
+}
